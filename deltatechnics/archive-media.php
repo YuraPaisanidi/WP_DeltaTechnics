374ac8d323687bb1f06e21c8b8f9c7120852678a
YuraPaisanidi <yura.paisanidi@gmail.com>
2021-06-16 15:34:02 +0300

    about + media update

--- deltatechnics/archive-media.php
+++ deltatechnics/archive-media.php
@@ -1,5 +1,6 @@
 <?php
 get_header();
+/* Template name: Медиа центр */
 ?>
 
 <main class="media-center main">
@@ -24,26 +25,20 @@ get_header();
 					<a href="<?php echo get_home_url(); ?>/media-cat/poleznaja-literatura/" class="media-center__item_btn">Показать все</a>
 				</div>
 				<div class="media-center__item_wrap">
-					<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
-
-						<a href="<?php the_permalink(); ?>" class="media-center__item_art">
-							<div class="media-center__item_img">
-								<?php if ( has_post_thumbnail() ) {
-									the_post_thumbnail();
-								} else { ?>
-									<img src="<?php echo get_template_directory_uri(); ?>/img/no.jpg" alt="<?php the_title(); ?>" />
-								<?php } ?>
-							</div>
-							<div class="media-center__item_name">
-								<p>
-									<?php the_title(); ?>
-								</p>
-							</div>
-						</a>
-
-					<?php endwhile; ?>
+				<?php
+					if ( have_posts() ) : // если имеются записи в блоге.
+						query_posts('media-cat=3');   // указываем ID рубрик, которые необходимо вывести.
+						while (have_posts()) : the_post();  // запускаем цикл обхода материалов блога
+					?>
+					<h1><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h1>
+				<?php the_content();
+						endwhile;  // завершаем цикл.
+					endif;
+					/* Сбрасываем настройки цикла. Если ниже по коду будет идти еще один цикл, чтобы не было сбоя. */
+					wp_reset_query();                
+					?>
 				</div>
-				<?php endif; ?>
+				
 
 			</div>
 
@@ -55,8 +50,21 @@ get_header();
 					<a href="<?php echo get_home_url(); ?>/media-cat/poleznye-stati/" class="media-center__item_btn">Показать все</a>
 				</div>
 				<div class="media-center__item_wrap">
-					<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
-
+				
+					<?php $current_page = (get_query_var('paged')) ? get_query_var('paged') : 1;
+						$params = array(
+							'posts_per_page' => 4, // количество постов на странице
+							'id' => 3, // количество постов на странице
+							'post_type'       => 'media', // тип постов
+							'paged'           => $current_page // текущая страница
+						);
+						query_posts($params);
+						
+						$wp_query->is_archive = true;
+						$wp_query->is_home = false;
+					?>
+					
+					<?php while(have_posts()): the_post(); ?>
 						<a href="<?php the_permalink(); ?>" class="media-center__item_art">
 							<div class="media-center__item_img">
 								<?php if ( has_post_thumbnail() ) {
@@ -71,10 +79,9 @@ get_header();
 								</p>
 							</div>
 						</a>
-
 					<?php endwhile; ?>
+				
 				</div>
-				<?php endif; ?>
 
 			</div>
 
